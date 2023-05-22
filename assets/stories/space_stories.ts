13285dc8c4087a121d28c196e936f00f4f9888e4
Victor Antunez <victorac94@hotmail.com>
2023-05-22 21:33:32 +0200

    Auto-generated commit [ci skip]

--- assets/stories/space_stories.ts
+++ assets/stories/space_stories.ts
@@ -5,44 +5,44 @@ export const spaceStories: IStory[] = [
         genre: 'space',
         id: 1,
         title: 'La esfera negra',
-        thumbnail: 'assets/images/la_esfera_negra/thumbnail.png',
+        thumbnail: 'assets/images/la_esfera_negra/thumbnail.webp',
         prompt: 'historia ciencia ficcion sobre el espacio',
         rootStory: {
-            image_desktop: 'assets/images/la_esfera_negra/desktop_main.png',
-            image_mobile: 'assets/images/la_esfera_negra/mobile_main.png',
-            text: 'La esfera negra flota silenciosamente en la oscuridad del espacio. La tripulación de la nave espacial Lane está intrigada por esta misteriosa formación, que no coincide con ningún objeto conocido. Finalmente deciden investigar más de cerca. Cuando tocan la superficie lisa de la esfera, súbitamente se encuentran absorbido dentro. Se encuentran parados en una extraña habitación cuadrada blanca sin puertas ni ventanas. El primer oficial Miller nota que las letras y números en su uniforme cambian constantemente. De repente una voz profunda surge de todas partes. La entidad se identifica como Zrel, un adepto a los rompecabezas de otra dimensión. Les dice que están atrapados en una simulación creada por Zrel para su entretenimiento. La única forma de escapar es resolver una serie de acertijos.',
+            image_desktop: 'assets/images/la_esfera_negra/desktop_main.webp',
+            image_mobile: 'assets/images/la_esfera_negra/mobile_main.webp',
+            text: 'La esfera negra flota silenciosamente en la oscuridad del espacio. La tripulación de la nave espacial Lane está intrigada por esta misteriosa formación, que no coincide con ningún objeto conocido. Finalmente deciden investigar más de cerca. Cuando tocan la superficie lisa de la esfera, súbitamente se encuentran absorbidos dentro. Se encuentran parados en una extraña habitación cuadrada blanca sin puertas ni ventanas. El primer oficial Miller nota que las letras y números en su uniforme cambian constantemente. De repente una voz profunda surge de todas partes. La entidad se identifica como Zrel, un adepto a los rompecabezas de otra dimensión. Les dice que están atrapados en una simulación creada por Zrel para su entretenimiento. La única forma de escapar es resolver una serie de acertijos.',
             optionA: {
-                image_desktop: 'assets/images/la_esfera_negra/desktop_a.png',
-                image_mobile: 'assets/images/la_esfera_negra/mobile_a.png',
+                image_desktop: 'assets/images/la_esfera_negra/desktop_a.webp',
+                image_mobile: 'assets/images/la_esfera_negra/mobile_a.webp',
                 optionText: 'Acertijo del océano espacial',
                 text: 'De repente, se encuentran parados en la cubierta de una nave pirata de madera, con olas y espuma que golpean contra el casco. El sol brilla sobre un mar azul infinito que se extiende en todas direcciones, incluso hacia arriba. El primer oficial Miller se da cuenta rápidamente de que ahora están flotando en el espacio profundo, pero su mente interpreta el vacío como un océano. La tripulación se divide en distintas soluciones: algunos quieren nadar lejos, otros bajar por el mástil o usar una barricada para rodear la cubierta en busca de una pista. Miller se da cuenta de que la clave está en mirar más allá de la ilusión. Le ordena a la tripulación detenerse y examinar el entorno real en lugar del simulado. Pronto encuentran una esfera con extraños símbolos flotando en el "agua". Al descifrarlos, rompen el acertijo, y el océano espacial desaparece.',
                 optionA: {
-                    image_desktop: 'assets/images/la_esfera_negra/desktop_a_a.png',
-                    image_mobile: 'assets/images/la_esfera_negra/mobile_a_a.png',
+                    image_desktop: 'assets/images/la_esfera_negra/desktop_a_a.webp',
+                    image_mobile: 'assets/images/la_esfera_negra/mobile_a_a.webp',
                     optionText: 'Final bueno',
                     text: 'La tripulación espera ansiosamente la libertad que Zrel les prometió cuando resolvieron todos sus acertijos. De repente, se encuentran parados en la cubierta de su nave una vez más, después de lo que parecieron años en la simulación. Apenas pueden comprender su extraña e intensa experiencia. Pero saben que los ha cambiado para siempre, pues han aprendido que la realidad solo existe en la mente y está limitada solo por la imaginación. Se lanzan a su próximo destino con renovada confianza, armados con una nueva claridad de pensamiento y una visión expandida del mundo. Ahora abrazarán cada encuentro con lo desconocido con una mentalidad abierta y una voluntad de imaginarse fuera de los límites de lo aparentemente posible. Miran hacia el infinito futuro con confianza, determinación y visión expandida, listos para experimentar nuevas verdades fuera de los confines de su propia realidad.'
                 },
                 optionB: {
-                    image_desktop: 'assets/images/la_esfera_negra/desktop_a_b.png',
-                    image_mobile: 'assets/images/la_esfera_negra/mobile_a_b.png',
+                    image_desktop: 'assets/images/la_esfera_negra/desktop_a_b.webp',
+                    image_mobile: 'assets/images/la_esfera_negra/mobile_a_b.webp',
                     optionText: 'Final malo',
                     text: 'Pero en lugar de la habitual felicitación de Zrel, reina el silencio. De repente se encuentran flotando en el espacio pero la nave Lane ya no está a la vista. La voz fría de Zrel les dice que solo eran ratas de laboratorio en sus cruel experimento, y que ahora que su propósito se cumplió, ya no los necesita. Los abandona a su suerte, flotando en el vasto y vacío espacio exterior. La tripulación queda aterrorizada ante la perspectiva de una muerte lenta y dolorosa sin la nave. Observan el universo infinito con ojos vacíos mientras aceptan su fatal destino, lamentando profundamente su curiosidad que los atrajo a este cruel destino después de tantas victorias. Ningún cuento de hadas puede desvanecer la oscuridad que ahora los rodea.'
                 }
             },
             optionB: {
-                image_desktop: 'assets/images/la_esfera_negra/desktop_b.png',
-                image_mobile: 'assets/images/la_esfera_negra/mobile_b.png',
+                image_desktop: 'assets/images/la_esfera_negra/desktop_b.webp',
+                image_mobile: 'assets/images/la_esfera_negra/mobile_b.webp',
                 optionText: 'Acertijo de la jungla numérica',
                 text: 'El entorno virtual cambia repentinamente a una espesa jungla tropical, con árboles altos, lianas colgantes y frondosa vegetación. Sin embargo, en lugar de hojas, todas las plantas están formadas por secuencias de números. Incluso los insectos que vuelan y los pájaros que canturan parecen estar hechos de ecuaciones matemáticas. Al principio esto confunde a la tripulación, ya que el entorno parece real a excepción de los números. Intentan reunir frutos numéricos y navegar entre la "vegetación" en busca de la salida. Pero nada funciona. Miller se da cuenta de que debido a que todo está construido a partir de números en lugar de materia orgánica, el acertijo debe tener una solución algebraica. Comienza a anotar ecuaciones basadas en los patrones de números que ve alrededor.',
                 optionA: {
-                    image_desktop: 'assets/images/la_esfera_negra/desktop_b_a.png',
-                    image_mobile: 'assets/images/la_esfera_negra/mobile_b_a.png',
+                    image_desktop: 'assets/images/la_esfera_negra/desktop_b_a.webp',
+                    image_mobile: 'assets/images/la_esfera_negra/mobile_b_a.webp',
                     optionText: 'Final bueno',
                     text: 'Después de un rato de ensayo y error, Miller logra resolver una compleja ecuación que hace que toda la jungla numérica comience a distorsionarse y fragmentarse a medida que la ilusión se desvanece. Los números se desvanecen en el aire como partículas de polvo brillante. Finalmente la tripulación se encuentra parada en la cubierta de su nave nuevamente, flotando en el espacio exterior. Zrel los felicita por resolver todos sus acertijos y superar su desafío mental. Les desea buena suerte en sus futuros viajes mientras la esfera negra comienza a alejarse silenciosamente en el espacio, volviéndose más pequeña hasta desaparecer. La tripulación ahora comprende mejor la fragilidad de la percepción y la belleza de los conceptos abstractos, lo que los ayudará en sus misiones. Mientras reanudan su viaje, saben que incluso con lo desconocido, siempre hay una solución simple y clara esperando para ser descubierta.'
                 },
                 optionB: {
-                    image_desktop: 'assets/images/la_esfera_negra/desktop_b_b.png',
-                    image_mobile: 'assets/images/la_esfera_negra/mobile_b_b.png',
+                    image_desktop: 'assets/images/la_esfera_negra/desktop_b_b.webp',
+                    image_mobile: 'assets/images/la_esfera_negra/mobile_b_b.webp',
                     optionText: 'Final malo',
                     text: 'Después de un rato de ensayo y error, Miller logra resolver una compleja ecuación que hace que toda la jungla numérica desaparezca. Pero en su lugar, surge una oscuridad total que traga a la tripulación. La voz burlona de Zrel suena de nuevo, revelando que solo estaba jugando otro engaño cruel. Ahora están atrapados en la nada para siempre, sus mentes distorsionadas mas allá de la comprensión mientras sus cuerpos se desmoronan en el vacío. La risa fría y sin emoción de Zrel es el último sonido que escuchan antes de perder todas las nociones del tiempo, el espacio y la propia existencia.'
                 }
